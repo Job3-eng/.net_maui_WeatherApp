929406568400fb0de6213af4a68b00e78d46b9a3
Job3-eng <jobbunanga@gmail.com>
2024-10-12 15:12:40 +0300

    Add project files.

---
+++ WeatherApp/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    internal class Constants
+    {
+        public const string API_KEY = "6a9cda85274d7273b56a963020afdd04";
+        public const string API_BASE_URL = "http://api.weatherstack.com/";
+    }
+}
